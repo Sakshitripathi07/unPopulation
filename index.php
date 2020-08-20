e4c5007c19d0259abc1f67e1e965d68103993c2d
Sakshi Tripathi <sakshi.tripathi@mountblue.tech>
2020-08-20 16:23:16 +0530

    initial commit

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
 include_once("index.html")
-include_once("app.css")
-include_once("app.js")
-include_once("task1.json")
-include_once("task2.json")
-include_once("task3.json")
-include_once("task4.json")
-
 
 ?>
